c3f6e272e64cae7f84ac1f93b3785768aaa5cc47
eyosi951 <eyosi1174@gmail.com>
2025-03-30 12:28:18 -0700

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/eportfolio",
   css:{
     modules:{
       localsConvention: 'camelCase',
